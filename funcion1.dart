bdaa967b62d7fd4479bb628c81b2256cdf383837
Zyanya Mireles <164534002+Zyanya-Mireles-1080@users.noreply.github.com>
2025-02-07 16:47:39 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+//función que no devuelve valor
+//función saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Zyanya Mireles');
+}
+
+void main(){
+  //Zyanya Mireles Mat: 22308051281080
+  //LLamar a la función saludar
+  print('Zyanya Mireles Mat: 22308051281080');
+  print('Llamando a la función saludar');
+  saludar();
+  print('Llamando a la función escribeNombre');
+  escribeNombre();
+}
