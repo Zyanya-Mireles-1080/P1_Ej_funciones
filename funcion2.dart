f157310e9a1771017a92f5373e6fd0583c86ba25
Zyanya Mireles <164534002+Zyanya-Mireles-1080@users.noreply.github.com>
2025-02-10 18:04:23 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,17 @@
+//funciones creadas por el usuario
+
+void suma(int a, int b) {
+  print('La suma es: ${a + b}');
+}
+
+void resta(int a, int b) {
+  print('La resta es: ${a - b}');
+}
+
+// función main
+void main(){
+  print('Zyanya Mireles Mat: 22308051281080');
+  print('Llamando a la función suma y resta');
+  suma(10, 5);
+  resta(10, 5);
+}
